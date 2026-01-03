b844b0a81c18016a16da7c11122a4c11c7b81642
Sanchez <gabriel.raknchez@gmail.com>
2026-01-03 22:54:06 +0800

    selection menu completed

--- MainMenu.cbl
+++ MainMenu.cbl
@@ -11,6 +11,7 @@
       *WS
        01  WS-MAIN-MENU-CHOICE                     PIC X.
        01  WS-SEQ-MENU-CHOICE                      PIC X(2).
+       01  WS-SEL-MENU-CHOICE                      PIC X(2).
 
        PROCEDURE DIVISION.
                PERFORM MAIN-MENU.
@@ -35,7 +36,7 @@
                    WHEN 1
                        PERFORM SEQUENCE-MENU
                    WHEN 2
-                       DISPLAY "YOU CHOSE SELECTION MENU"
+                       PERFORM SELECTION-MENU
                    WHEN 3 
                        DISPLAY "YOU CHOSE ITERATION MENU"
                    WHEN 4
@@ -194,13 +195,158 @@
                    PERFORM EXIT-PROMPT
 
                    WHEN 11
-                       DISPLAY "EXITING..."
+                       DISPLAY "EXITING... SEQUENCE MENU"
 
                    WHEN OTHER 
                        DISPLAY "INVALID INPUT, TYPE ANOTHER"
 
                END-EVALUATE
            END-PERFORM
+           PERFORM EXIT-PROMPT
+           EXIT PARAGRAPH.
+
+      *SELECTION
+       SELECTION-MENU.
+           ACCEPT UTIL-OS-NAME FROM ENVIRONMENT "OS"
+           
+           PERFORM UNTIL WS-SEL-MENU-CHOICE = 11
+           PERFORM CLEAR-SCREEN
+           DISPLAY "========================"
+           DISPLAY "     SELECTION MENU     "
+           DISPLAY "========================"
+           DISPLAY "1 - REVISED GRADES PASSED OR FAILED"
+           DISPLAY "2 - POSITIVE OR NEGATIVE"
+           DISPLAY "3 - EVEN OR ODD"
+           DISPLAY "4 - LARGER OF TWO NUMBERS"
+           DISPLAY "5 - SMALLEST OF THREE NUMBERS"
+           DISPLAY "6 - REVISED GRADES WITH EQUIVALENT"
+           DISPLAY "7 - TEMPERATURE INFORMATION"
+           DISPLAY "8 - REVISED SALESMAN INFORMATION WITH COMISSION"
+           DISPLAY "9 - NUMBER - DAY EQUIVALENT"
+           DISPLAY "10 - VOWEL OR CONSONANT"
+           DISPLAY "11 - EXIT SELECTION MENU"
+           DISPLAY "PLEASE ENTER YOUR CHOICE: " WITH NO ADVANCING
+           ACCEPT WS-SEL-MENU-CHOICE
+
+           EVALUATE WS-SEL-MENU-CHOICE
+               WHEN 1
+                   PERFORM CLEAR-SCREEN
+
+                   IF UTIL-OS-NAME = "Windows_NT"
+                   MOVE "cd ..\selection & sel_1.exe" TO UTIL-PROG-PATH
+                   ELSE
+                   MOVE "cd ../selection && ./sel_1" TO UTIL-PROG-PATH
+                   END-IF
+                   CALL "SYSTEM" USING UTIL-PROG-PATH
+
+                   PERFORM EXIT-PROMPT
+               WHEN 2
+                   PERFORM CLEAR-SCREEN
+
+                   IF UTIL-OS-NAME = "Windows_NT"
+                   MOVE "cd ..\selection & sel_2.exe" TO UTIL-PROG-PATH
+                   ELSE
+                   MOVE "cd ../selection && ./sel_2" TO UTIL-PROG-PATH
+                   END-IF
+                   CALL "SYSTEM" USING UTIL-PROG-PATH
+
+                   PERFORM EXIT-PROMPT
+               WHEN 3
+                   PERFORM CLEAR-SCREEN
+
+                   IF UTIL-OS-NAME = "Windows_NT"
+                   MOVE "cd ..\selection & sel_3.exe" TO UTIL-PROG-PATH
+                   ELSE
+                   MOVE "cd ../selection && ./sel_3" TO UTIL-PROG-PATH
+                   END-IF
+                   CALL "SYSTEM" USING UTIL-PROG-PATH
+
+                   PERFORM EXIT-PROMPT
+               WHEN 4
+                   PERFORM CLEAR-SCREEN
+
+                   IF UTIL-OS-NAME = "Windows_NT"
+                   MOVE "cd ..\selection & sel_4.exe" TO UTIL-PROG-PATH
+                   ELSE
+                   MOVE "cd ../selection && ./sel_4" TO UTIL-PROG-PATH
+                   END-IF
+                   CALL "SYSTEM" USING UTIL-PROG-PATH
+
+                   PERFORM EXIT-PROMPT
+               WHEN 5
+                   PERFORM CLEAR-SCREEN
+
+                   IF UTIL-OS-NAME = "Windows_NT"
+                   MOVE "cd ..\selection & sel_5.exe" TO UTIL-PROG-PATH
+                   ELSE
+                   MOVE "cd ../selection && ./sel_5" TO UTIL-PROG-PATH
+                   END-IF
+                   CALL "SYSTEM" USING UTIL-PROG-PATH
+
+                   PERFORM EXIT-PROMPT
+               WHEN 6 
+                   PERFORM CLEAR-SCREEN
+
+                   IF UTIL-OS-NAME = "Windows_NT"
+                   MOVE "cd ..\selection & sel_6.exe" TO UTIL-PROG-PATH
+                   ELSE
+                   MOVE "cd ../selection && ./sel_6" TO UTIL-PROG-PATH
+                   END-IF
+                   CALL "SYSTEM" USING UTIL-PROG-PATH
+
+                   PERFORM EXIT-PROMPT
+               WHEN 7
+                   PERFORM CLEAR-SCREEN
+
+                   IF UTIL-OS-NAME = "Windows_NT"
+                   MOVE "cd ..\selection & sel_7.exe" TO UTIL-PROG-PATH
+                   ELSE
+                   MOVE "cd ../selection && ./sel_7" TO UTIL-PROG-PATH
+                   END-IF
+                   CALL "SYSTEM" USING UTIL-PROG-PATH
+
+                   PERFORM EXIT-PROMPT
+               WHEN 8
+                   PERFORM CLEAR-SCREEN
+
+                   IF UTIL-OS-NAME = "Windows_NT"
+                   MOVE "cd ..\selection & sel_8.exe" TO UTIL-PROG-PATH
+                   ELSE
+                   MOVE "cd ../selection && ./sel_8" TO UTIL-PROG-PATH
+                   END-IF
+                   CALL "SYSTEM" USING UTIL-PROG-PATH
+
+                   PERFORM EXIT-PROMPT
+               WHEN 9
+                   PERFORM CLEAR-SCREEN
+
+                   IF UTIL-OS-NAME = "Windows_NT"
+                   MOVE "cd ..\selection & sel_9.exe" TO UTIL-PROG-PATH
+                   ELSE
+                   MOVE "cd ../selection && ./sel_9" TO UTIL-PROG-PATH
+                   END-IF
+                   CALL "SYSTEM" USING UTIL-PROG-PATH
+
+                   PERFORM EXIT-PROMPT
+               WHEN 10
+                   PERFORM CLEAR-SCREEN
+
+                   IF UTIL-OS-NAME = "Windows_NT"
+                   MOVE "cd ..\selection & sel_10.exe" TO UTIL-PROG-PATH
+                   ELSE
+                   MOVE "cd ../selection && ./sel_10" TO UTIL-PROG-PATH
+                   END-IF
+                   CALL "SYSTEM" USING UTIL-PROG-PATH
+
+                   PERFORM EXIT-PROMPT
+               WHEN 11
+                   DISPLAY "EXITING... SELECTION MENU"
+               WHEN OTHER
+                   DISPLAY "INVALID INPUT, TRY AGAIN"
+           END-EVALUATE
+
+           END-PERFORM
+           
            PERFORM EXIT-PROMPT
            EXIT PARAGRAPH.
 
